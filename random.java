396d734a4e0968dbb9b1133c9743b430e72b4b42
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-08-17 19:15:43 +0530

    Assignment 3

--- random.java
+++ random.java
@@ -3,3 +3,37 @@ It is a multiplayer game so program takes 2 users as input. Both play the 3 time
 And program displays the Winner name?  
 (hint: use java.util.Random class)*/
 
+import java.util.Random;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		System.out.println("The Game has began!")
+		Random rand = new Random(); 
+  
+        // Generate random integers in range 0 to 999 
+        int user_1 = rand.nextInt(1000); 
+        int user_2 = rand.nextInt(1000); 
+        
+        user_1+=rand.nextInt(1000);
+        user_2+=rand.nextInt(1000);
+        
+        user_1+=rand.nextInt(1000);
+        user_2+=rand.nextInt(1000);
+        
+        System.out.println("User 1 score: " +user_1);
+		System.out.println("User 2 score: " +user_2);
+		
+		if(user_1>user_2){
+		    System.out.println("The winner is: " +user_1);
+		}
+		else if (user_2>user_1){
+		    System.out.println("The winner is: " +user_2);
+		}
+		else{
+		    System.out.println("The game ended up in a draw!"");
+		}
+	}
+}
+
