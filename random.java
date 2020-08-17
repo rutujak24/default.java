5f2ff0e76e44a8e1c9480864a378508a00503687
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-08-17 18:54:10 +0530

    Assignment 3

---
+++ random.java
@@ -0,0 +1,5 @@
+/*Assignment: A new game has to be invented by you. Program generates random number between 0 to 1000. 
+It is a multiplayer game so program takes 2 users as input. Both play the 3 times and one with maximum score wins. 
+And program displays the Winner name?  
+(hint: use java.util.Random class)*/
+
